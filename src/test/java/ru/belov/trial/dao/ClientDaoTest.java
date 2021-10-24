66f54fd330fa8c383f1d2eab156dda5cc1badd64
a.belov <whiterabbit.nsk@gmail.com>
2021-10-24 19:57:15 +0300

    Старт презентации

--- src/test/java/ru/belov/trial/dao/ClientDaoTest.java
+++ src/test/java/ru/belov/trial/dao/ClientDaoTest.java
@@ -1,66 +1,25 @@
 package ru.belov.trial.dao;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import ru.belov.trial.constant.ClientType;
 import ru.belov.trial.dao.configuration.MybatisTestConfiguration;
-import ru.belov.trial.dao.configuration.TestCaseConfiguration;
 import ru.belov.trial.entity.ClientEntity;
 import ru.belov.trial.infrastructure.LocalizedString;
-import test.TestCaseHelper;
-import test.mapper.ClientGMapper;
-import test.model.ClientG;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.belov.trial.constant.Language.RUS;
 import static ru.belov.trial.dao.util.CommonValuesUtil.randomCode;
 import static ru.belov.trial.dao.util.CommonValuesUtil.randomText;
 
 @Transactional
-@SpringBootTest(classes = {MybatisTestConfiguration.class, TestCaseConfiguration.class})
+@SpringBootTest(classes = {MybatisTestConfiguration.class})
 class ClientDaoTest {
-    private static final String CITY_CODE = randomCode();
 
     @Autowired
     ClientDao clientDao;
-    @Autowired
-    ClientGMapper clientGMapper;
-    @Autowired
-    TestCaseHelper testCaseHelper;
-
-    @Test
-    public void save_fullObjectSave_allFieldSaved() {
-        //given
-        var city = testCaseHelper.createCity();
-        city.setCode(CITY_CODE);
-        testCaseHelper.completeTestCase();
-
-        var clientEntity = generateClientEntity();
-        //when
-        clientDao.save(clientEntity);
-        //then
-        var result = clientGMapper.selectByPrimaryKey(clientEntity.getUuid()).orElse(new ClientG());
-
-        assertEquals(clientEntity.getUuid(), result.getUuid());
-        assertEquals(clientEntity.getName(), result.getName());
-    }
-
-    @Test
-    public void findByUuid_1RowInTable_1RowWithSameFieldsFoundByUuid() {
-        //given
-        var city = testCaseHelper.createCity();
-        var client = testCaseHelper.createClient(city, ClientType.UR);
-        testCaseHelper.completeTestCase();
-        //when
-        var result = clientDao.findByUuid(client.getUuid());
-        //then
-        assertEquals(client.getUuid(), result.getUuid());
-        assertEquals(client.getName(), result.getName());
-    }
 
     private ClientEntity generateClientEntity() {
         return ClientEntity.builder()
@@ -68,7 +27,7 @@ class ClientDaoTest {
                 .name(LocalizedString.from(RUS, randomText()))
                 .phoneNumber(randomCode())
                 .clientTypeCode(ClientType.UR.getCode())
-                .cityCode(CITY_CODE)
+                .cityCode(randomCode())
                 .build();
     }
 }
--- src/test/java/test/TestCaseHelper.java
+++ src/test/java/test/TestCaseHelper.java
@@ -1,55 +1,17 @@
 package test;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import ru.belov.trial.constant.ClientType;
-import ru.belov.trial.infrastructure.LocalizedString;
-import test.mapper.CityGMapper;
-import test.mapper.ClientGMapper;
-import test.model.CityG;
-import test.model.ClientG;
 
 import java.util.*;
 import java.util.function.Supplier;
-
-import static ru.belov.trial.constant.Language.RUS;
-import static ru.belov.trial.dao.util.CommonValuesUtil.randomCode;
-import static ru.belov.trial.dao.util.CommonValuesUtil.randomText;
 
 @Component
 public class TestCaseHelper {
 
     private final Map<Class, List<Supplier<Integer>>> insertQueriesCallMap = new HashMap<>();
     private final List<Class> insertionOrder = Arrays.asList(
-            CityG.class, ClientG.class
+
     );
-
-    @Autowired
-    CityGMapper cityGMapper;
-    @Autowired
-    ClientGMapper clientGMapper;
-
-    public CityG createCity() {
-        CityG city = new CityG();
-        city.setCode(randomCode());
-        city.setName(LocalizedString.from(RUS, randomText()));
-
-        putEntity(city, () -> cityGMapper.insert(city));
-        return city;
-    }
-
-    public ClientG createClient(CityG city, ClientType clientType) {
-        ClientG client = new ClientG();
-        client.setUuid(UUID.randomUUID());
-        client.setCityCode(city.getCode());
-        client.setClientTypeCode(clientType.getCode());
-        client.setActive(true);
-        client.setName(LocalizedString.from(RUS, randomText()));
-        client.setPhoneNumber(randomCode());
-
-        putEntity(client, () -> clientGMapper.insert(client));
-        return client;
-    }
 
     public void completeTestCase() {
         var entryList = new ArrayList<>(insertQueriesCallMap.entrySet());
